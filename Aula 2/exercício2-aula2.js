e28704409899feff3dbc1b1f040988401ccb0593
Amanda Neri <168038088+NeriAmanda@users.noreply.github.com>
2025-05-08 16:59:18 -0300

    Delete Aula 2 directory

--- Aula 2/exercício2-aula2.js
+++
@@ -1,5 +0,0 @@
-// Exercício 2 - Desconto Progressivo
-// Descrição: Crie um programa que peça ao usuário o valor de uma compra. Se o valor for maior que 100, aplique 10% de desconto. Caso contrário o desconto será de 5%.
-
-let compra = ("Qual o valor da sua compra?  ")
-
