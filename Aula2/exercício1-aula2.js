d27e852704664e368087b66a5e616485fccfbdb4
Amanda Neri <168038088+NeriAmanda@users.noreply.github.com>
2025-05-11 12:06:56 -0300

    Update exercício1-aula2.js

--- Aula2/exercício1-aula2.js
+++ Aula2/exercício1-aula2.js
@@ -1,22 +1,16 @@
 // Exercício 1 - Boas-vindas
 // Descrição: Crie um programa que solicite ao usuário os seguintes dados: nome, idade , altura, gênero e estudante.
 
-let nome
-let idade
-let altura
-let gênero
-let estudante
+let nome = prompt("Seu nome: ");
+let idade = Number(prompt("Sua idade: "));
+let altura = Number(prompt("Sua altura: "));
+let gênero = prompt("Seu gênero: ");
+let estudante = prompt("Você é estudante? (sim/não): ");
 
-nome = prompt("Seu nome:  ")
-idade = prompt("Sua idade: ")
-altura = prompt("Sua altura: ")
-gênero = prompt("Seu gênero: ")
-estudante = prompt("Você é estudante? ")
-
-console.log("Bem-vindo(a)!" + nome)
-console.log("Seu Perfil:")
-console.log(nome)
-console.log(idade)
-console.log(altura)
-console.log(gênero)
-console.log(estudante)
+console.log("Bem-vindo(a), " + nome + "!");
+console.log("Seu Perfil:");
+console.log(nome);
+console.log(idade + " anos");
+console.log(altura);
+console.log(gênero);
+console.log(estudante);
